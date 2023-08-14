f6c8a68a20acfc3cd1382a102586f65c61e13885
Daniel Buchan <clone@myself.com>
2023-08-14 14:18:10 +0100

    test example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import * as data from './data.js';
+import React from 'react';
+import * as $3Dmol from '3dmol/build/3Dmol.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class Model extends React.Component{
+  constructor (props){
+      super(props);
+      this.state = {
+        pdb_data: data.pdb_data,
+      }
+      this.model = React.createRef();
+      console.log(this.state.pdb_data);
+//      this.display_structure(this.model, this.state.pdb_data)
+    }
+
+  componentDidMount(prevProps) {
+    this.display_structure(this.model.current, this.state.pdb_data);
+  }
+
+  display_structure = (mol_container, pdb_data) =>
+  {
+    let hotspot_color = function(atom){
+      if(atom.b === 1.0){atom.color = 'red'; return 'red';}
+      if(atom.b === 0.5){atom.color = 'black'; return 'black';}
+      if(atom.b === 50){atom.color = 'white'; return 'white';}
+      if(atom.b === 100){atom.color = 'red'; return 'red';}
+      atom.color = 'blue';
+      return("blue");
+    };
+    let element = mol_container;
+    let config = { backgroundColor: '#ffffff' };
+    let viewer = $3Dmol.createViewer( element, config );
+    viewer.addModel( pdb_data, "pdb" );                       /* load data */
+    viewer.setStyle({}, {cartoon: {colorfunc: hotspot_color}});  /* style all atoms */
+    viewer.addSurface($3Dmol.SurfaceType.VDW, {'opacity':0.8, colorscheme: 'whiteCarbon'});
+    viewer.zoomTo();                                      /* set camera */
+    viewer.render();                                      /* render scene */
+    viewer.zoom(1.7, 3000);  
+  }
+
+  render(){
+    return(
+        <div>
+            <div className="row">
+              <div className="col-sm-7 col-sm-offset-5">
+                <div id="container-01" ref={this.model} className="mol-container" style={{width: "600px",height: "600px", position: "relative"}}></div>
+              </div>
+            </div>
+        </div>
+    )}
 }
 
-export default App;
+export default Model;
